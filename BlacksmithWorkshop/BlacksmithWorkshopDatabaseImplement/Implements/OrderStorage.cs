73c788a49dc36a3da06bb90001e39ac3802aa169
Timofey Kochetkov <pochta30052001@gmail.com>
2021-05-02 23:35:07 +0400

    fix storage

--- BlacksmithWorkshop/BlacksmithWorkshopDatabaseImplement/Implements/OrderStorage.cs
+++ BlacksmithWorkshop/BlacksmithWorkshopDatabaseImplement/Implements/OrderStorage.cs
@@ -73,8 +73,10 @@ namespace BlacksmithWorkshopDatabaseImplement.Implements
             }
             using (var context = new BlacksmithWorkshopDatabase())
             {
-                var order = context.Orders.FirstOrDefault(rec => rec.Id == model.Id);
-                return order != null ? new OrderViewModel
+                var order = context.Orders.Include(rec => rec.Manufacture)
+                .FirstOrDefault(rec => rec.Id == model.Id || rec.Id == model.Id);
+                return order != null ?
+                new OrderViewModel
                 {
                     Id = order.Id,
                     ManufactureId = order.ManufactureId,
@@ -95,7 +97,16 @@ namespace BlacksmithWorkshopDatabaseImplement.Implements
         {
             using (var context = new BlacksmithWorkshopDatabase())
             {
-                context.Orders.Add(CreateModel(model, new Order()));
+                Order order = new Order
+                {
+                    ManufactureId = model.ManufactureId,
+                    Count = model.Count,
+                    Sum = model.Sum,
+                    Status = model.Status,
+                    DateCreate = model.DateCreate,
+                    DateImplement = model.DateImplement 
+                };
+                context.Orders.Add(order);
                 context.SaveChanges();
             }
         
@@ -107,9 +118,14 @@ namespace BlacksmithWorkshopDatabaseImplement.Implements
                 var element = context.Orders.FirstOrDefault(rec => rec.Id == model.Id);
                 if (element == null)
                 {
-                    throw new Exception("Element not found");
+                    throw new Exception("Элемент не найден");
                 }
-                CreateModel(model, element);
+                element.ManufactureId = model.ManufactureId;
+                element.Count = model.Count;
+                element.Sum = model.Sum;
+                element.Status = model.Status;
+                element.DateCreate = model.DateCreate;
+                element.DateImplement = model.DateImplement;
                 context.SaveChanges();
             }
         }
@@ -125,7 +141,7 @@ namespace BlacksmithWorkshopDatabaseImplement.Implements
                 }
                 else
                 {
-                    throw new Exception("Element not found");
+                    throw new Exception("Элемент не найден");
                 }
             }
         }
