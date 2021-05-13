8c4ec00de37ee4f7255ec2a6aa36568454535e26
Timofey Kochetkov <pochta30052001@gmail.com>
2021-05-13 15:31:32 +0400

    4dop lab is ready!

---
+++ BlacksmithWorkshop/BlacksmithWorkshopView/FormReportOrderByDate.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace BlacksmithWorkshopView
+{
+    partial class FormReportOrderByDate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonToPdf = new System.Windows.Forms.Button();
+            this.ButtonMake = new System.Windows.Forms.Button();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.dateTimePickerTo = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerFrom = new System.Windows.Forms.DateTimePicker();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // ButtonToPdf
+            // 
+            this.ButtonToPdf.Location = new System.Drawing.Point(626, 28);
+            this.ButtonToPdf.Name = "ButtonToPdf";
+            this.ButtonToPdf.Size = new System.Drawing.Size(123, 23);
+            this.ButtonToPdf.TabIndex = 12;
+            this.ButtonToPdf.Text = "В Pdf";
+            this.ButtonToPdf.UseVisualStyleBackColor = true;
+            this.ButtonToPdf.Click += new System.EventHandler(this.ButtonToPdf_Click);
+            // 
+            // ButtonMake
+            // 
+            this.ButtonMake.Location = new System.Drawing.Point(433, 24);
+            this.ButtonMake.Name = "ButtonMake";
+            this.ButtonMake.Size = new System.Drawing.Size(162, 29);
+            this.ButtonMake.TabIndex = 11;
+            this.ButtonMake.Text = "Сформировать";
+            this.ButtonMake.UseVisualStyleBackColor = true;
+            this.ButtonMake.Click += new System.EventHandler(this.ButtonCreate_Click);
+            // 
+            // labelTo
+            // 
+            this.labelTo.AutoSize = true;
+            this.labelTo.Location = new System.Drawing.Point(225, 33);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(24, 13);
+            this.labelTo.TabIndex = 10;
+            this.labelTo.Text = "По:";
+            // 
+            // labelFrom
+            // 
+            this.labelFrom.AutoSize = true;
+            this.labelFrom.Location = new System.Drawing.Point(20, 34);
+            this.labelFrom.Name = "labelFrom";
+            this.labelFrom.Size = new System.Drawing.Size(14, 13);
+            this.labelFrom.TabIndex = 9;
+            this.labelFrom.Text = "С";
+            // 
+            // dateTimePickerTo
+            // 
+            this.dateTimePickerTo.Location = new System.Drawing.Point(255, 30);
+            this.dateTimePickerTo.Name = "dateTimePickerTo";
+            this.dateTimePickerTo.Size = new System.Drawing.Size(149, 20);
+            this.dateTimePickerTo.TabIndex = 8;
+            // 
+            // dateTimePickerFrom
+            // 
+            this.dateTimePickerFrom.Location = new System.Drawing.Point(40, 30);
+            this.dateTimePickerFrom.Name = "dateTimePickerFrom";
+            this.dateTimePickerFrom.Size = new System.Drawing.Size(170, 20);
+            this.dateTimePickerFrom.TabIndex = 7;
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "BlacksmithWorkshopView.OrderReportByDate.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(15, 60);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.ServerReport.BearerToken = null;
+            this.reportViewer.Size = new System.Drawing.Size(758, 367);
+            this.reportViewer.TabIndex = 13;
+            // 
+            // FormReportOrderByDate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer);
+            this.Controls.Add(this.ButtonToPdf);
+            this.Controls.Add(this.ButtonMake);
+            this.Controls.Add(this.labelTo);
+            this.Controls.Add(this.labelFrom);
+            this.Controls.Add(this.dateTimePickerTo);
+            this.Controls.Add(this.dateTimePickerFrom);
+            this.Name = "FormReportOrderByDate";
+            this.Text = "Заказы по дате";
+            this.Load += new System.EventHandler(this.FormReportOrderByDate_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ButtonToPdf;
+        private System.Windows.Forms.Button ButtonMake;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.DateTimePicker dateTimePickerTo;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFrom;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+    }
+}
